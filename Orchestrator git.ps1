065ead0be15fccca72f67fe7cf77915fd703df7a
MazdaProg <107762489+sepuhMazda@users.noreply.github.com>
2026-07-28 15:12:08 +0700

    Update SMB modules path for network configuration

--- Orchestrator git.ps1
+++ Orchestrator git.ps1
@@ -82,7 +82,8 @@ if ($isAdmin) {
 # ==========================================
 
 # Tentukan path SMB modules jaringan kantor Anda
-$ModulesFolder = "\\10.37.10.116\j4\public\allsystem\hnf\j4deploy\Modules"
+#$ModulesFolder = "\\10.37.10.116\j4\public\allsystem\hnf\j4deploy\Modules"
+$ModulesFolder = "\\10.37.11.222\hnf\j4deploy\Modules"
 
 if (-not (Test-Path $ModulesFolder)) {
     Write-Host "[ERROR] Folder modul jaringan tidak ditemukan: $ModulesFolder" -ForegroundColor Red
